6d9d34e6c3b3eeae438e82820cffaeac7d29485f
Anjan Chakraborty <anjanchakraborty2023@gmail.com>
2026-09-18 13:38:04 +0530

    intregating clerk authentication and update App component

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import config from "./config/config.js"
+import { clerkMiddleware } from '@clerk/express'
 const app=express()
 const corsOrgin={
     origin:config.FROENTEND_URL,
@@ -10,7 +11,7 @@ const corsOrgin={
 //Middleware
 app.use(express.json())
 app.use(cors(corsOrgin))
-
+app.use(clerkMiddleware())
 app.get("/health",(req,res)=>{
     res.status(200).json({message:"ok"})
 })
